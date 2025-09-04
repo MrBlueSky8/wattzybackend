2e0d78c430b22935d4645a64492728b4d9fa41c2
MrBlueSky <116617108+MrBlueSky8@users.noreply.github.com>
2025-09-03 22:51:59 -0500

    first commit

---
+++ src/main/java/pe/edu/upc/wattzybackend/WattzybackendApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.wattzybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WattzybackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WattzybackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/upc/wattzybackend/WattzybackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.wattzybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WattzybackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
